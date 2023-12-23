1417033238f225f748e1f4988cf165486a723536
Mohmmad Adel <mohammadAdel8727@gmail.com>
2023-12-23 05:40:06 -0800

    F

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     new_word = ""
 
     for i in my_string:
-        if (i != 'c' or i != 'C'):
+        if (i != 'c' and i != 'C'):
             new_word += i
 
     return new_word
