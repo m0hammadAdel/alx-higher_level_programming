aca0713affe83e0a27129526088a1b913070ed94
Mohmmad Adel <mohammadAdel8727@gmail.com>
2023-12-23 06:05:28 -0800

    fxed some shit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,15 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
 
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=' ')
-        print()
+    for sub_mat in matrix:
+        if len(sub_mat) == 0:
+            print()
+
+        for i in range(len(sub_mat)):
+            print("{:d}".format(sub_mat[i]),
+                  end='\n' if i is len(sub_mat) - 1 else ' ')
+
+#    for i in range(len(matrix)):
+#        for j in range(len(matrix[i])):
+#            print("{:d}".format(matrix[i][j]), end=' ')
+#        print()
