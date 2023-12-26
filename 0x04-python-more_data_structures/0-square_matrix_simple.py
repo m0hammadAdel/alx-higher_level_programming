c8af700960f46c0f6b3e186270c9b10c89f2fce5
Mohmmad Adel <mohammadAdel8727@gmail.com>
2023-12-26 07:30:55 -0800

    a

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,6 +6,3 @@ def square_matrix_simple(matrix=[]):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
     return (new_matrix)
-
-#def square_matrix_simple(matrix=[]):
-#    return list(map(lambda one_d_list: list(map(lambda x: x ** 2), one_d_list)), matrix)
